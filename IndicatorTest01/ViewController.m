b2abe5e50d9000ef4ebaacadf4e376079552e2b4
Chae Yungi <l8673901@gmail.com>
2014-01-03 15:43:54 +0900

    인디케이터 테스트입니다.
    인디케이터 테스트입니다.

--- IndicatorTest01/ViewController.m
+++ IndicatorTest01/ViewController.m
@@ -9,15 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indi;
+@property (strong, nonatomic) UIActivityIndicatorView *indi2;
 
 @end
 
 @implementation ViewController
+- (IBAction)stopIndicator:(id)sender {
+	[_indi stopAnimating];
+	
+	[_indi2 performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
+}
+
+- (IBAction)startIndicator:(id)sender {
+	
+	[_indi startAnimating];
+	
+	[_indi2 performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];
+}
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	
+	
 	// Do any additional setup after loading the view, typically from a nib.
+	_indi2 = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+	_indi2.hidesWhenStopped = YES;
+	_indi2.center = CGPointMake(160, 250);
+	
+	[self.view addSubview:_indi2];
+	
 }
 
 - (void)didReceiveMemoryWarning
